5cccac9717791ef6209b09eb8a2f379a9f3cbdc3
masha makeeva <dacotacreative@mail.ru>
2022-08-19 11:06:02 +0700

    added changes

---
+++ js/index.js
@@ -0,0 +1,29 @@
+class FullMenu{
+    constructor(selector) {
+        this.menu = document.querySelector(selector)
+
+        document.addEventListener('click', (e) => {
+            const target = e.target.closest('[data-menu]')
+
+
+            if (target) {
+                e.preventDefault();
+                const event = target.dataset.menu
+                
+                this[event]()
+            }
+        })
+    }
+
+    open() {
+        this.menu.classList.add('open')
+    }
+
+    close() {
+        this.menu.classList.remove('open')
+    }
+}
+
+var menu = new FullMenu('#full-menu')
+
+console.log(menu)
