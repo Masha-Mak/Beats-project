320c938fe98c61fec947a5eff4c111c7860cb67c
masha makeeva <dacotacreative@mail.ru>
2022-08-26 10:29:19 +0700

    corrected mistakes

---
+++ js/slider.js
@@ -0,0 +1,14 @@
+const slider = $('.products__list').bxSlider({
+    pager: false,
+    controls: false
+});
+
+$('.product-slider__arrow--direction--prev').click(e => {
+    e.preventDefault();
+    slider.goToPrevSlide();
+})
+
+$('.product-slider__arrow--direction--next').click(e => {
+    e.preventDefault();
+    slider.goToNextSlide();
+})
